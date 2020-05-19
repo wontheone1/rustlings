88a6d8629a9036e26337d1134325a6cdbc1a961e
Won Seob Seo <pghoou@gmail.com>
2020-05-19 21:29:33 +0300

    Solve primitive types chapter

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,10 +2,12 @@
 // Create an array with at least 100 elements in it where the ??? is. 
 // Execute `rustlings hint primitive_types3` for hints!
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    let a = ["Are we there yet?"; 110];
+
+    // for item in a.iter() {
+    //     println! ("{}", item);
+    // }
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -2,13 +2,17 @@
 // Get a slice out of Array a where the ??? is so that the test passes.
 // Execute `rustlings hint primitive_types4` for hints!!
 
-// I AM NOT DONE
-
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
+    // let a = [1, 2, 3, 4, 5].deref(); // Q: Why this doesn't work?
+    // A: the coercion from arrays to slices doesn't happen via dereference
+    // it happens via unsizing, a completely different mechanism
+    // the coercion [T; N] -> [T] happens through the same means as Type -> dyn Trait
+    // refer https://doc.rust-lang.org/std/marker/trait.Unsize.html
 
-    let nice_slice = ???
+    let nice_slice:[i32; 3] = [2,3,4]; // this works too
+    let nice_slice = &a[1..4]; // this works too
 
     assert_eq!([2, 3, 4], nice_slice)
 }
