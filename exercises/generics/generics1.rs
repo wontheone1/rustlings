f6be8b16b8f534ecaf23869df532328029fee90d
Won Seob Seo <pghoou@gmail.com>
2020-05-21 15:09:11 +0300

    Solve generics

--- exercises/generics/generics1.rs
+++ exercises/generics/generics1.rs
@@ -1,10 +1,10 @@
 // This shopping list program isn't compiling! 
 // Use your knowledge of generics to fix it.
 
-// I AM NOT DONE
-
 fn main() {
-    let mut shopping_list: Vec<?> = Vec::new();
+    let mut shopping_list: Vec<&str> = Vec::new();
     shopping_list.push("milk");
 }
 
+// Vectors in rust make use of generics to create dynamically sized arrays of any type.
+// You need to tell the compiler what type we are pushing onto this vector.
--- exercises/generics/generics3.rs
+++ exercises/generics/generics3.rs
@@ -6,14 +6,32 @@
 // Make the necessary code changes to support alphabetical report cards, thereby making the second
 // test pass.
 
-// I AM NOT DONE
-pub struct ReportCard {
-    pub grade: f32,
+pub struct ReportCard<T: std::fmt::Display> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+// impl<T: std::fmt::Display> std::fmt::Display for ReportCard<T> {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         write!(f, "{} ({}) - achieved a grade of {}", &self.student_name, &self.student_age, &self.grade)
+//     }
+// }
+
+// impl std::fmt::Display for ReportCard<String> {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         write!(f, "{} ({}) - achieved a grade of {}", &self.student_name, &self.student_age, &self.grade)
+//     }
+// }
+//
+// impl std::fmt::Display for ReportCard<f32> {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         write!(f, "{} ({}) - achieved a grade of {}", &self.student_name, &self.student_age, &self.grade)
+//     }
+// }
+
+// for methods that need `Display` trait
+impl<T: std::fmt::Display> ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}", &self.student_name, &self.student_age, &self.grade)
     }
@@ -37,10 +55,17 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1, 
+            grade: "A+".to_string(),
             student_name: "Gary Plotter".to_string(), 
             student_age: 11,
         };
         assert_eq!(report_card.print(), "Gary Plotter (11) - achieved a grade of A+");
     }
 }
+
+// To find the best solution to this challenge you're going to need to think back to your
+// knowledge of traits, specifically Trait Bound Syntax -  you may also need this: "use std::fmt::Display;"
+//
+// This is definitely harder than the last two exercises! You need to think about not only making the
+// ReportCard struct generic, but also the correct property - you will need to change the implementation
+// of the struct slightly too...you can do it!
