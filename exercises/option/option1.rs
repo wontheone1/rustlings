d2bc5558fb320e3ae4e40aa11b9a7944f23d2487
Won Seob Seo <pghoou@gmail.com>
2020-05-21 09:45:02 +0300

    Solve options

--- exercises/option/option1.rs
+++ exercises/option/option1.rs
@@ -1,23 +1,33 @@
 // option1.rs
 // Make me compile! Execute `rustlings hint option1` for hints
 
-// I AM NOT DONE
-
 // you can modify anything EXCEPT for this function's sig
 fn print_number(maybe_number: Option<u16>) {
     println!("printing: {}", maybe_number.unwrap());
 }
 
 fn main() {
-    print_number(13);
-    print_number(99);
+    print_number(Some(13));
+    print_number(Some(99));
 
-    let mut numbers: [Option<u16>; 5];
+    let mut numbers: [Option<u16>; 5] = [None; 5];
     for iter in 0..5 {
         let number_to_add: u16 = {
-            ((iter * 5) + 2) / (4 * 16)
+            (iter * 5) + 2
         };
 
-        numbers[iter as usize] = number_to_add;
+        numbers[iter as usize] = Some(number_to_add);
+        print_number(numbers[iter as usize]);
     }
 }
+
+// hint
+// Hint 1: Check out some functions of Option:
+// is_some
+// is_none
+// unwrap
+//
+// and:
+// pattern matching
+//
+// Hint 2: There are no sensible defaults for the value of an Array; the values need to be filled before use.
