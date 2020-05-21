29de6c64c15346217ddb06a1ab0f8a8468ae1a88
Won Seob Seo <pghoou@gmail.com>
2020-05-21 10:04:19 +0300

    Solve clippy

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -6,12 +6,12 @@
 // check clippy's suggestions from the output to solve the exercise.
 // Execute `rustlings hint clippy1` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
     let x = 1.2331f64;
     let y = 1.2332f64;
-    if y != x {
+    if (y - x).abs() > f64::EPSILON {
         println!("Success!");
     }
 }
+
+// Floating point calculations are usually imprecise, so asking if two values are exactly equal is asking for trouble
